66132cd6e3aee894a84c20a5cae8bc81d7082c31
George-J-Morris <george.joshua.morris@gmail.com>
2024-11-23 16:24:30 +0000

    use full version of bootstrap for testing

--- frontend/index.js
+++ frontend/index.js
@@ -1,43 +1,67 @@
-import * as esbuild from 'esbuild'
-import * as sass from 'sass-embedded'
-import { PurgeCSS } from 'purgecss'
-import * as fs from 'node:fs'
-import * as fsP from 'node:fs/promises'
+import * as esbuild from "esbuild";
+import * as sass from "sass-embedded";
+import { PurgeCSS } from "purgecss";
+import * as fs from "node:fs";
+import * as fsP from "node:fs/promises";
+
+// Flags
+const prod = false;
 
 await esbuild.build({
-    entryPoints: ['./src/crypto.ts'],
-    bundle: true,
-    minify: true,
-    platform: 'browser',
-    outfile: '../release/assets/js/index.js',
-})
-
-console.log("ESBuild complete")
-
-fsP.mkdir("./tmp")
-
-const bootstrap = sass.compile("./scss/theme.scss", { silenceDeprecations: ['mixed-decls', 'color-functions', 'global-builtin', 'import'] })
-
-fs.writeFile("./tmp/bootstrap.css", bootstrap.css, function (err) {
-    if (err) {
-        fsP.rm("./tmp", { recursive: true, force: true })
-        return console.log(err);
-    }
+  entryPoints: ["./src/crypto.ts"],
+  bundle: true,
+  minify: true,
+  platform: "browser",
+  outfile: "../release/assets/js/index.js",
 });
 
-const bootstrapPurged = await new PurgeCSS().purge({
-    content: ['../views/*.templ', '../components/*.templ'],
-    css: ['./tmp/bootstrap.css'],
-    safelist: {deep:[/dropdown/],}
-})
+console.log("ESBuild complete");
 
-fsP.rm("./tmp", { recursive: true, force: true })
+const bootstrap = sass.compile("./scss/theme.scss", {
+  silenceDeprecations: [
+    "mixed-decls",
+    "color-functions",
+    "global-builtin",
+    "import",
+  ],
+});
 
-fs.writeFile("../release/assets/css/bootstrap.css", bootstrapPurged[0].css, function (err) {
+if (prod == true) {
+  fsP.mkdir("./tmp");
+
+  fs.writeFile("./tmp/bootstrap.css", bootstrap.css, function (err) {
     if (err) {
-        return console.log(err);
+      fsP.rm("./tmp", { recursive: true, force: true });
+      return console.log(err);
     }
+  });
 
-    console.log("purgeCSS complete")
+  const bootstrapPurged = await new PurgeCSS().purge({
+    content: ["../views/*.templ", "../components/*.templ"],
+    css: ["./tmp/bootstrap.css"],
+    safelist: { deep: [/dropdown/] },
+  });
 
-}); 
+  fsP.rm("./tmp", { recursive: true, force: true });
+
+  fs.writeFile(
+    "../release/assets/css/bootstrap.css",
+    bootstrapPurged[0].css,
+    function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("purgeCSS complete");
+    }
+  );
+} else {
+  fs.writeFile(
+    "../release/assets/css/bootstrap.css",
+    bootstrap.css,
+    function (err) {
+      if (err) {
+        return console.log(err);
+      }
+    }
+  );
+}
