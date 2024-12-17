c9d3e617e0ad1009f32e23a078b8135c7bad6e1b
George-J-Morris <george.joshua.morris@gmail.com>
2024-12-17 12:54:19 +0000

    🦹 remove superuser specific methods

--- internal/db.go
+++ internal/db.go
@@ -2,43 +2,15 @@ package internal
 
 import (
 	"context"
-	"fmt"
 
 	_ "github.com/jackc/pgerrcode"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-type suAccount struct {
-	usernamename string
-	password     string
-}
-
-var urlExample1 string = ""
+var urlExample1 string = "postgresql://postgres:123@localhost:5432/test?sslmode=disable"
 
 var database *pgxpool.Pool
-
-func GetSuperUserByName(username string, connPool *pgxpool.Pool) suAccount {
-
-	var resp suAccount
-
-	query := "SELECT * FROM users WHERE username = $1"
-
-	err := connPool.Ping(context.Background())
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	row := connPool.QueryRow(context.Background(), query, username)
-
-	err = row.Scan(&resp.usernamename, &resp.password)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	return resp
-}
 
 func InitDBPool() error {
 	config, err := pgxpool.ParseConfig(urlExample1)
